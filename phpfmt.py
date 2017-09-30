595cf0607f1c19594483e5dcccedf0f10188594d
six <six@cc>
2017-09-30 17:07:23 +0800

    support unsaved file

--- phpfmt.py
+++ phpfmt.py
@@ -4,20 +4,42 @@ import os
 
 class PhpFmtCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
+
+		window = sublime.active_window()
+		view = self.view
+		file_extension = window.extract_variables()['file_extension']
+		if file_extension.lower()!='php':
+			window.status_message('file not php')
+			return
+		
+		regions = view.sel()
+		if len(regions) > 1 or not regions[0].empty():
+			for region in view.sel():
+				if not region.empty():
+					s = view.substr(region)
+					view.replace(edit, region, self.format(s))
+		else:  # format all text
+			alltextreg = sublime.Region(0, view.size())
+			s = view.substr(alltextreg)
+			view.replace(edit, alltextreg, self.format(s))
+
+	def format(self, text):
+		fName='~tmp.php'
+		fs=open(fName,"w")
+		fs.write(text)
+		fs.close()
+
 		sublime.status_message("formating")
 		path=os.path.dirname(__file__)
-		file=self.view.file_name()
-		if not file:
-			sublime.error_message("file not found")
-			return
 
-		p=subprocess.Popen("php \"%s\\fmt.php\" -o=- --psr \"%s\""%(path,file),shell=True,stdout=subprocess.PIPE)
-		# p.wait()
-		ret=p.stdout.readlines()
-		ret=map(lambda x:x.decode("utf8","ignore"),ret)
-		if(ret==''):
-			sublime.error_message("format error check encode is utf8")
-			return
+		p=subprocess.Popen("php \"%s\\fmt.php\" -o=- --psr \"%s\""%(path,fName),shell=True,stdout=subprocess.PIPE)
+		arr=p.stdout.readlines()
+		os.remove(fName)
+		ss=''
+		for line in arr:
+			ss+=str(line.decode("utf8","ignore"))
 
-		self.view.replace(edit,sublime.Region(0,self.view.size()),"".join(ret))
-		sublime.status_message("success")
+		if(ss==''):
+			raise Exception('check php file is utf-8')
+
+		return ss
